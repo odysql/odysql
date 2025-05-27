e0c288e277fa48571c1233fbaf223f1cf13baddc
dark-person <66205094+dark-person@users.noreply.github.com>
2025-05-27 15:16:43 +0800

    Create some test case

---
+++ src/test/java/com/github/odysql/builders/SQLSelectBuilderTest.java
@@ -0,0 +1,312 @@
+package com.github.odysql.builders;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.github.odysql.helpers.PreparedStatementFiller;
+import com.github.odysql.models.SQLCondition;
+import com.github.odysql.models.SQLParameter;
+
+class SQLSelectBuilderTest {
+
+    @Test
+    void testBuilderNormal() {
+        SQLSelectBuilder builder = new SQLSelectBuilder()
+                .select("field1")
+                .select("field2")
+                .distinct()
+                .from("table1")
+                .innerJoin("table2", SQLCondition.create("table1.id = table2.id"))
+                .leftJoin("table3", SQLCondition.create("table2.value = table3.value"))
+                .where(SQLCondition.create("table1.id = 123"))
+                .orderBy("field1")
+                .limit(1);
+
+        assertEquals(
+                "SELECT DISTINCT field1, field2 FROM table1 "
+                        + "INNER JOIN table2 ON table1.id = table2.id "
+                        + "LEFT JOIN table3 ON table2.value = table3.value "
+                        + "WHERE table1.id = 123 ORDER BY field1 LIMIT 1",
+                builder.toSQL());
+    }
+
+    @Test
+    void testBuilderPrepare() {
+        SQLSelectBuilder builder = new SQLSelectBuilder()
+                .select("field1").select("field2").distinct()
+                .from("table1")
+                .leftJoin("table2", SQLCondition.create("table1.id = table2.id"))
+                .where(SQLCondition.create("table1.id = ? AND table1.name = ?"))
+                .param(SQLParameter.of(123), SQLParameter.of("abc"))
+                .orderBy("field1")
+                .limitOffset(1, 10);
+
+        assertEquals("SELECT DISTINCT field1, field2 FROM table1 "
+                + "LEFT JOIN table2 ON table1.id = table2.id "
+                + "WHERE table1.id = 123 AND table1.name = 'abc' ORDER BY field1 LIMIT 1 OFFSET 10",
+                PreparedStatementFiller.asDebugSQL(builder.toSQL(), builder.getParams()));
+
+        assertEquals("SELECT DISTINCT field1, field2 FROM table1 "
+                + "LEFT JOIN table2 ON table1.id = table2.id "
+                + "WHERE table1.id = ? AND table1.name = ? ORDER BY field1 LIMIT 1 OFFSET 10",
+                builder.toSQL());
+
+        // -------------------------------------
+
+        SQLSelectBuilder builder2 = new SQLSelectBuilder()
+                .select("field1").select("field2").distinct()
+                .from("table1")
+                .leftJoin("table2", SQLCondition.create("table1.id = table2.id"))
+                .where(SQLCondition
+                        .create("table1.id = ?")
+                        .and("table1.name = ?"))
+                .param(
+                        SQLParameter.of(123),
+                        SQLParameter.of("abc"))
+                .orderBy("field1")
+                .limit(1);
+
+        assertEquals("SELECT DISTINCT field1, field2 FROM table1 "
+                + "LEFT JOIN table2 ON table1.id = table2.id "
+                + "WHERE table1.id = 123 AND table1.name = 'abc' ORDER BY field1 LIMIT 1",
+                PreparedStatementFiller.asDebugSQL(builder2.toSQL(), builder2.getParams()));
+
+        assertEquals("SELECT DISTINCT field1, field2 FROM table1 "
+                + "LEFT JOIN table2 ON table1.id = table2.id "
+                + "WHERE table1.id = ? AND table1.name = ? ORDER BY field1 LIMIT 1",
+                builder2.toSQL());
+    }
+
+    @Test
+    void testBuilderWith() {
+        SQLSelectBuilder temp = new SQLSelectBuilder()
+                .select("col_wq")
+                .from("w1")
+                .where(SQLCondition.create("w1.id = ?"))
+                .param(SQLParameter.of(123))
+                .limitOffset(20, 10);
+
+        SQLSelectBuilder builder = new SQLSelectBuilder()
+                .with("tmp", temp)
+                .select("field1").select("field2").distinct()
+                .from("table1")
+                .leftJoin("table2", SQLCondition.create("table1.id = table2.id"))
+                .where(SQLCondition.create("table1.id = ? AND table1.name = ?"))
+                .param(SQLParameter.of(456), SQLParameter.of("def"))
+                .orderBy("field1")
+                .limit(1);
+
+        assertEquals("WITH tmp AS (SELECT col_wq FROM w1 WHERE w1.id = ? LIMIT 20 OFFSET 10) "
+                + "SELECT DISTINCT field1, field2 FROM table1 "
+                + "LEFT JOIN table2 ON table1.id = table2.id "
+                + "WHERE table1.id = ? AND table1.name = ? ORDER BY field1 LIMIT 1",
+                builder.toSQL());
+
+        assertEquals("WITH tmp AS (SELECT col_wq FROM w1 WHERE w1.id = 123 LIMIT 20 OFFSET 10) "
+                + "SELECT DISTINCT field1, field2 FROM table1 "
+                + "LEFT JOIN table2 ON table1.id = table2.id "
+                + "WHERE table1.id = 456 AND table1.name = 'def' ORDER BY field1 LIMIT 1",
+                PreparedStatementFiller.asDebugSQL(builder.toSQL(), builder.getParams()));
+
+        // ------------------------------------
+
+        SQLSelectBuilder temp2 = new SQLSelectBuilder()
+                .select("col_wq")
+                .from("w1")
+                .where(SQLCondition.create("w1.id = ?"))
+                .param(SQLParameter.of(123))
+                .limit(20);
+
+        SQLSelectBuilder builder2 = new SQLSelectBuilder()
+                .with("tmp", temp2)
+                .select("field1").select("field2").distinct()
+                .from("table1")
+                .leftJoin("table2", SQLCondition.create("table1.id = table2.id"))
+                .where(SQLCondition.create("table1.id = ? AND table1.name = ?"))
+                .param(SQLParameter.of(456), SQLParameter.of("def"))
+                .orderBy("field1")
+                .limit(1);
+
+        assertEquals("WITH tmp AS (SELECT col_wq FROM w1 WHERE w1.id = ? LIMIT 20) "
+                + "SELECT DISTINCT field1, field2 FROM table1 "
+                + "LEFT JOIN table2 ON table1.id = table2.id "
+                + "WHERE table1.id = ? AND table1.name = ? ORDER BY field1 LIMIT 1",
+                builder2.toSQL());
+
+        assertEquals("WITH tmp AS (SELECT col_wq FROM w1 WHERE w1.id = 123 LIMIT 20) "
+                + "SELECT DISTINCT field1, field2 FROM table1 "
+                + "LEFT JOIN table2 ON table1.id = table2.id "
+                + "WHERE table1.id = 456 AND table1.name = 'def' ORDER BY field1 LIMIT 1",
+                PreparedStatementFiller.asDebugSQL(builder2.toSQL(), builder2.getParams()));
+    }
+
+    @Test
+    void testBuilderMultipleWith() {
+        SQLSelectBuilder temp = new SQLSelectBuilder()
+                .select("col_wq")
+                .from("w1")
+                .where(SQLCondition.create("w1.id = ?"))
+                .param(SQLParameter.of(123))
+                .limit(20);
+
+        SQLSelectBuilder temp2 = new SQLSelectBuilder()
+                .select("col_wq")
+                .from("w2")
+                .where(SQLCondition.create("w2.name = ?"))
+                .param(SQLParameter.of("abc"))
+                .limit(20);
+
+        SQLSelectBuilder builder = new SQLSelectBuilder()
+                .with("tmp", temp)
+                .with("tmp2", temp2)
+                .select("field1").select("field2").distinct()
+                .from("table1")
+                .leftJoin("table2", SQLCondition.create("table1.id = table2.id"))
+                .where(SQLCondition.create("table1.id = ? AND table1.name = ?"))
+                .param(SQLParameter.of(456), SQLParameter.of("def"))
+                .orderBy("field1")
+                .limit(1);
+
+        assertEquals("WITH tmp AS (SELECT col_wq FROM w1 WHERE w1.id = ? LIMIT 20), "
+                + "tmp2 AS (SELECT col_wq FROM w2 WHERE w2.name = ? LIMIT 20) "
+                + "SELECT DISTINCT field1, field2 FROM table1 "
+                + "LEFT JOIN table2 ON table1.id = table2.id "
+                + "WHERE table1.id = ? AND table1.name = ? ORDER BY field1 LIMIT 1",
+                builder.toSQL());
+
+        assertEquals("WITH tmp AS (SELECT col_wq FROM w1 WHERE w1.id = 123 LIMIT 20), "
+                + "tmp2 AS (SELECT col_wq FROM w2 WHERE w2.name = 'abc' LIMIT 20) "
+                + "SELECT DISTINCT field1, field2 FROM table1 "
+                + "LEFT JOIN table2 ON table1.id = table2.id "
+                + "WHERE table1.id = 456 AND table1.name = 'def' ORDER BY field1 LIMIT 1",
+                PreparedStatementFiller.asDebugSQL(builder.toSQL(), builder.getParams()));
+
+        // ----------------------------
+
+        SQLSelectBuilder temp3 = new SQLSelectBuilder()
+                .select("col_wq")
+                .from("w1")
+                .where(SQLCondition.create("w1.id = ?"))
+                .param(SQLParameter.of(123))
+                .limit(20);
+
+        SQLSelectBuilder temp4 = new SQLSelectBuilder()
+                .select("col_wq")
+                .from("w2")
+                .where(SQLCondition.create("w2.name = ?"))
+                .param(SQLParameter.of("abc"))
+                .limit(20);
+
+        SQLSelectBuilder builder2 = new SQLSelectBuilder()
+                .with("tmp", temp3)
+                .with("tmp2", temp4)
+                .select("field1").select("field2").distinct()
+                .from("table1")
+                .leftJoin("table2", SQLCondition.create("table1.id = table2.id"))
+                .where(SQLCondition.create("table1.id = ? AND table1.name = ?"))
+                .param(SQLParameter.of(456), SQLParameter.of("def"))
+                .orderBy("field1")
+                .limit(1);
+
+        assertEquals("WITH tmp AS (SELECT col_wq FROM w1 WHERE w1.id = ? LIMIT 20), "
+                + "tmp2 AS (SELECT col_wq FROM w2 WHERE w2.name = ? LIMIT 20) "
+                + "SELECT DISTINCT field1, field2 FROM table1 "
+                + "LEFT JOIN table2 ON table1.id = table2.id "
+                + "WHERE table1.id = ? AND table1.name = ? ORDER BY field1 LIMIT 1",
+                builder2.toSQL());
+
+        assertEquals("WITH tmp AS (SELECT col_wq FROM w1 WHERE w1.id = 123 LIMIT 20), "
+                + "tmp2 AS (SELECT col_wq FROM w2 WHERE w2.name = 'abc' LIMIT 20) "
+                + "SELECT DISTINCT field1, field2 FROM table1 "
+                + "LEFT JOIN table2 ON table1.id = table2.id "
+                + "WHERE table1.id = 456 AND table1.name = 'def' ORDER BY field1 LIMIT 1",
+                PreparedStatementFiller.asDebugSQL(builder2.toSQL(), builder2.getParams()));
+    }
+
+    @Test
+    void testBuilderNestedWith() {
+        SQLSelectBuilder temp1 = new SQLSelectBuilder()
+                .select("col_wq")
+                .from("w1")
+                .where(SQLCondition.create("w1.id = ?"))
+                .param(SQLParameter.of(123))
+                .limit(20);
+
+        SQLSelectBuilder temp2 = new SQLSelectBuilder()
+                .with("tmp1", temp1)
+                .select("col_wq")
+                .from("w2")
+                .where(SQLCondition.create("w2.name = ?"))
+                .param(SQLParameter.of("abc"))
+                .limit(20);
+
+        SQLSelectBuilder builder = new SQLSelectBuilder()
+                .with("tmp2", temp2)
+                .select("field1").select("field2").distinct()
+                .from("table1")
+                .leftJoin("table2", SQLCondition.create("table1.id = table2.id"))
+                .where(SQLCondition.create("table1.id = ? AND table1.name = ?"))
+                .param(SQLParameter.of(456), SQLParameter.of("def"))
+                .orderBy("field1")
+                .limit(1);
+
+        assertEquals("WITH tmp2 AS ("
+                + "WITH tmp1 AS (SELECT col_wq FROM w1 WHERE w1.id = ? LIMIT 20) "
+                + "SELECT col_wq FROM w2 WHERE w2.name = ? LIMIT 20) "
+                + "SELECT DISTINCT field1, field2 FROM table1 "
+                + "LEFT JOIN table2 ON table1.id = table2.id "
+                + "WHERE table1.id = ? AND table1.name = ? ORDER BY field1 LIMIT 1",
+                builder.toSQL());
+
+        assertEquals("WITH tmp2 AS ("
+                + "WITH tmp1 AS (SELECT col_wq FROM w1 WHERE w1.id = 123 LIMIT 20) "
+                + "SELECT col_wq FROM w2 WHERE w2.name = 'abc' LIMIT 20) "
+                + "SELECT DISTINCT field1, field2 FROM table1 "
+                + "LEFT JOIN table2 ON table1.id = table2.id "
+                + "WHERE table1.id = 456 AND table1.name = 'def' ORDER BY field1 LIMIT 1",
+                PreparedStatementFiller.asDebugSQL(builder.toSQL(), builder.getParams()));
+
+        // ------------------------------------
+        SQLSelectBuilder temp3 = new SQLSelectBuilder()
+                .select("col_wq")
+                .from("w1")
+                .where(SQLCondition.create("w1.id = ?"))
+                .param(SQLParameter.of(123))
+                .limit(20);
+
+        SQLSelectBuilder temp4 = new SQLSelectBuilder()
+                .with("tmp1", temp3)
+                .select("col_wq")
+                .from("w2")
+                .where(SQLCondition.create("w2.name = ?"))
+                .param(SQLParameter.of("abc"))
+                .limit(20);
+
+        SQLSelectBuilder builder2 = new SQLSelectBuilder()
+                .with("tmp2", temp4)
+                .select("field1").select("field2").distinct()
+                .from("table1")
+                .leftJoin("table2", SQLCondition.create("table1.id = table2.id"))
+                .where(SQLCondition.create("table1.id = ? AND table1.name = ?"))
+                .param(SQLParameter.of((long) 456), SQLParameter.of("def"))
+                .orderBy("field1")
+                .limit(1);
+
+        assertEquals("WITH tmp2 AS ("
+                + "WITH tmp1 AS (SELECT col_wq FROM w1 WHERE w1.id = ? LIMIT 20) "
+                + "SELECT col_wq FROM w2 WHERE w2.name = ? LIMIT 20) "
+                + "SELECT DISTINCT field1, field2 FROM table1 "
+                + "LEFT JOIN table2 ON table1.id = table2.id "
+                + "WHERE table1.id = ? AND table1.name = ? ORDER BY field1 LIMIT 1",
+                builder2.toSQL());
+
+        assertEquals("WITH tmp2 AS ("
+                + "WITH tmp1 AS (SELECT col_wq FROM w1 WHERE w1.id = 123 LIMIT 20) "
+                + "SELECT col_wq FROM w2 WHERE w2.name = 'abc' LIMIT 20) "
+                + "SELECT DISTINCT field1, field2 FROM table1 "
+                + "LEFT JOIN table2 ON table1.id = table2.id "
+                + "WHERE table1.id = 456 AND table1.name = 'def' ORDER BY field1 LIMIT 1",
+                PreparedStatementFiller.asDebugSQL(builder2.toSQL(), builder2.getParams()));
+    }
+}
---
+++ src/test/java/com/github/odysql/models/SQLConditionTest.java
@@ -0,0 +1,174 @@
+package com.github.odysql.models;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class SQLConditionTest {
+
+    static <T> List<T> createList(T[] items) {
+        List<T> list = new ArrayList<>();
+
+        for (T item : items) {
+            list.add(item);
+        }
+
+        return list;
+    }
+
+    void testIsEmpty() {
+        assertEquals(true, SQLCondition.isEmpty(null));
+        assertEquals(true, SQLCondition.isEmpty(SQLCondition.create(" ")));
+        assertEquals(true, SQLCondition.isEmpty(SQLCondition.create("")));
+
+        assertEquals(false, SQLCondition.isEmpty(SQLCondition.create("col1 = ?")));
+    }
+
+    @Test
+    void testAnd() {
+        // Test SQLCondition.and() in four possibility
+
+        // Both Non-empty
+        assertEquals(
+                "col1 = 12 AND col2 = '23'",
+                SQLCondition.create("col1 = 12").and("col2 = '23'").asSQL());
+
+        // Any one empty
+        assertEquals(
+                "col1 = 12",
+                SQLCondition.create("col1 = 12").and(SQLCondition.empty()).asSQL());
+        assertEquals(
+                "col2 = '23'",
+                SQLCondition.empty().and("col2 = '23'").asSQL());
+
+        // Both are empty
+        assertEquals(
+                "",
+                SQLCondition.empty().and(SQLCondition.empty()).asSQL());
+
+        // -------------------------------------------------
+
+        // Special case for empty SQLCondition created manually
+        assertEquals(
+                "col1 = 12",
+                SQLCondition.create("col1 = 12").and(SQLCondition.create("")).asSQL());
+
+        assertEquals(
+                "col1 = 12",
+                SQLCondition.create("col1 = 12").and(SQLCondition.create("   ")).asSQL());
+    }
+
+    @Test
+    void testOr() {
+        // Test SQLCondition.or() in four possibility
+
+        // Both Non-empty
+        assertEquals(
+                "col1 = 12 OR col2 = '23'",
+                SQLCondition.create("col1 = 12").or("col2 = '23'").asSQL());
+
+        // Any one empty
+        assertEquals(
+                "col1 = 12",
+                SQLCondition.create("col1 = 12").or(SQLCondition.empty()).asSQL());
+        assertEquals(
+                "col2 = '23'",
+                SQLCondition.empty().or("col2 = '23'").asSQL());
+
+        // Both are empty
+        assertEquals(
+                "",
+                SQLCondition.empty().or(SQLCondition.empty()).asSQL());
+
+        // -------------------------------------------------
+
+        // Special case for empty SQLCondition created manually
+        assertEquals(
+                "col1 = 12",
+                SQLCondition.create("col1 = 12").or(SQLCondition.create("")).asSQL());
+        assertEquals(
+                "col1 = 12",
+                SQLCondition.create("col1 = 12").or(SQLCondition.create("   ")).asSQL());
+    }
+
+    @Test
+    void testPickByFlag() {
+        SQLCondition c1 = SQLCondition.create("col1 = 12");
+        SQLCondition c2 = SQLCondition.create("col2 = '23'");
+
+        assertEquals(c1, SQLCondition.pickByFlag(true, c1, c2));
+        assertEquals(c2, SQLCondition.pickByFlag(false, c1, c2));
+        assertEquals(SQLCondition.empty(), SQLCondition.pickByFlag(null, c1, c2));
+    }
+
+    @Test
+    void testIn() {
+        assertEquals(
+                "col1 IN ('1','2','3')",
+                SQLCondition.in("col1", createList(new String[] { "1", "2", "3" })).asSQL());
+
+        assertEquals(
+                "col1 IN ('1')",
+                SQLCondition.in("col1", createList(new String[] { "1" })).asSQL());
+
+        assertEquals(
+                "col1 IN (1,2,3)",
+                SQLCondition.in("col1", createList(new Integer[] { 1, 2, 3 })).asSQL());
+
+        assertEquals(
+                "col1 IN (1)",
+                SQLCondition.in("col1", createList(new Integer[] { 1 })).asSQL());
+
+        assertThrowsExactly(
+                IllegalArgumentException.class,
+                () -> SQLCondition.in("col1", createList(new Double[] { 1.0 })));
+
+        assertThrowsExactly(
+                IllegalArgumentException.class,
+                () -> SQLCondition.in("col1", new ArrayList<String>()));
+    }
+
+    @Test
+    void testNotIn() {
+        assertEquals(
+                "col1 NOT IN ('1','2','3')",
+                SQLCondition.notIn("col1", createList(new String[] { "1", "2", "3" })).asSQL());
+
+        assertEquals(
+                "col1 NOT IN ('1')",
+                SQLCondition.notIn("col1", createList(new String[] { "1" })).asSQL());
+
+        assertEquals(
+                "col1 NOT IN (1,2,3)",
+                SQLCondition.notIn("col1", createList(new Integer[] { 1, 2, 3 })).asSQL());
+
+        assertEquals(
+                "col1 NOT IN (1)",
+                SQLCondition.notIn("col1", createList(new Integer[] { 1 })).asSQL());
+
+        assertThrowsExactly(
+                IllegalArgumentException.class,
+                () -> SQLCondition.notIn("col1", createList(new Double[] { 1.0 })));
+
+        assertThrowsExactly(
+                IllegalArgumentException.class,
+                () -> SQLCondition.notIn("col1", new ArrayList<String>()));
+    }
+
+    @Test
+    void testEqTo() {
+        assertEquals("col1||col3 = col2||col4", SQLCondition.eqTo("col1||col3", "col2||col4").asSQL());
+
+        assertEquals(
+                "col1||col3 = 'a very long text that may be needed to handle'",
+                SQLCondition.eqTo("col1||col3", "'a very long text that may be needed to handle'")
+                        .asSQL());
+
+        assertThrows(IllegalArgumentException.class, () -> SQLCondition.eqTo("col1", "?"));
+        assertThrows(IllegalArgumentException.class, () -> SQLCondition.eqTo("?", "col2"));
+        assertThrows(IllegalArgumentException.class, () -> SQLCondition.eqTo("?", "?"));
+    }
+}
